0a07c638a29c30878506a671c987ade93b700e63
Baldeep Kang <bkang4612@gmail.com>
2020-08-16 13:56:13 -0700

    change to prompt type List for license question

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ inquirer .prompt([
         name: "description"
     },
     {
-        type: "input",
+        type: "list",
+        name: "license",
         message: "What kind of license should your project have?",
-        name: "license"
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     },
     {
         type: "input",
