8d0f72923ac1d29a5d2d5b268b598f44947835f1
Baldeep Kang <bkang4612@gmail.com>
2020-08-16 23:16:25 -0700

    added headings for user input in markdown template

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,22 +3,40 @@ function generateMarkdown(data){
 # ${data.projectName}
 
 ## Description
+
 ${data.description}
 
 ## License
+
 This project is licensed under the ${data.license} license.
 
 ## Installation
+
 To install necessary dependencies, run the following command:
 
     ${data.commandDependencies}
-    
+
+## Usage 
+
+${data.usageRepo}
+
+## License
+
+This project is licensed under the ${data.license}
+
+## Contributing
+
+${data.contributeRepo}
 
 ## Tests
 To run tests run the following command:
 
-    ${data.license.choices}
-    `
+    ${data.license}
+
+## Questions 
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.useremail}. You can find more of my work at https://github.com/${data.githubUsername}.
+`
 }
 
 module.exports = generateMarkdown;
